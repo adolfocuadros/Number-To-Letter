d99c0a7ed9de507d890d957a353de152ba16e0ae
Jorge Andrade <j.andrade.dev@gmail.com>
2014-10-02 15:17:45 -0500

    Subiendo proyecto

---
+++ src/Andradedev/Numbertoletter/Facades/NumberToLetterFacade.php
@@ -0,0 +1,9 @@
+<?php namespace Andradedev\Numbertoletter\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class NumberToLetterFacade extends Facade {
+
+    protected static function getFacadeAccessor() { return 'numbertoletter'; }
+
+}
---
+++ src/Andradedev/Numbertoletter/NumbertoletterServiceProvider.php
@@ -0,0 +1,38 @@
+<?php namespace Andradedev\Numbertoletter;
+
+use Illuminate\Support\ServiceProvider;
+
+class NumbertoletterServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bindShared('numbertoletter', function () {
+
+			return new NumberToLetter;
+
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
